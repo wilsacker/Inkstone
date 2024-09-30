53c6713291449dd2c471d22e4cfd547960b69a4f
wilsacker <williamsuttona@gmail.com>
2024-09-29 20:27:41 -0700

    fixing errors

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,64 +1,71 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
-const { InjectManifest } = require('workbox-webpack-plugin');
 const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "production",
     entry: {
-      main: './src/js/index.js', // Path relative to the client folder
-      install: './src/js/install.js' // Path relative to the client folder
+      main: './src/js/index.js',  // Adjusted path
+      install: './src/js/install.js',  // Adjusted path
     },
     output: {
       filename: '[name].bundle.js',
-      path: path.resolve(__dirname, '../dist'), // Adjusted to output to the dist folder in the root
-      publicPath: '/',
+      path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      
       new HtmlWebpackPlugin({
-        template: './index.html', // Path relative to the client folder
-        title: 'JATE',
-      }),
-      new HtmlWebpackPlugin({
-        template: './offline.html', // Path to your offline HTML
-        filename: 'offline.html', // Output filename
+        template: './index.html',  // Adjusted path for the HTML template
+        title: 'J.A.T.E',
       }),
       new InjectManifest({
-        swSrc: './src/js/src-sw.js', // Path relative to the client folder
-        swDest: 'service-worker.js'
+        swSrc: './src-sw.js',  // Adjusted path for service worker
+        swDest: 'src-sw.js',
       }),
       new WebpackPwaManifest({
+        fingerprints: false,
         name: 'Just Another Text Editor',
         short_name: 'JATE',
-        description: 'A text editor that works offline.',
-        background_color: '#225ca3',
-        theme_color: '#225ca3',
-        fingerprints: false,
-        inject: true,
-        icons: [{
-          src: path.resolve('src/images/logo.png'), // Path relative to the client folder
-          sizes: [96, 128, 192, 256, 384, 512]
-        }]
-      })
+        description: 'Takes notes with JavaScript syntax highlighting!',
+        background_color: '#225cca',
+        theme_color: '#225cca',
+        start_url: '/',
+        publicPath: '/',
+        inject: true, 
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),  // Path for the logo
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+            filename: ({ size }) => `icon_${size}x${size}.png`,  // Custom filename based on the size from the array
+          },
+        ],
+      }),
     ],
     module: {
       rules: [
         {
-          test: /\.css$/,
+          test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
         },
         {
-          test: /\.js$/,
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,  // Handle image files
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
           exclude: /node_modules/,
           use: {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env'] // Transpile ES6 to ES5
-            }
-          }
-        }
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-transform-runtime'],
+            },
+          },
+        },
       ],
-    }
+    },
   };
 };
