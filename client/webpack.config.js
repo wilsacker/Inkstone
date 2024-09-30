88ce905f8ed843c6d12f87fe7206eaf41dab978a
wilsacker <williamsuttona@gmail.com>
2024-09-29 20:45:29 -0700

    updated webpack and serrvice worker

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,54 +5,49 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
-    mode: "production",
+    mode: 'development',
     entry: {
-      main: './src/js/index.js',  // Adjusted path
-      install: './src/js/install.js',  // Adjusted path
+      main: './src/js/index.js',
+      install: './src/js/install.js'
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
       new HtmlWebpackPlugin({
-        template: './index.html',  // Adjusted path for the HTML template
-        title: 'J.A.T.E',
+        template: './index.html',
+        title: 'J.A.T.E'
       }),
       new InjectManifest({
-        swSrc: './src-sw.js',  // Adjusted path for service worker
+        swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
       new WebpackPwaManifest({
         fingerprints: false,
+        inject: true,
         name: 'Just Another Text Editor',
-        short_name: 'JATE',
+        short_name: 'J.A.T.E',
         description: 'Takes notes with JavaScript syntax highlighting!',
-        background_color: '#225cca',
-        theme_color: '#225cca',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
         start_url: '/',
         publicPath: '/',
-        inject: true, 
         icons: [
           {
-            src: path.resolve('src/images/logo.png'),  // Path for the logo
+            src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('assets', 'icons'),
-            filename: ({ size }) => `icon_${size}x${size}.png`,  // Custom filename based on the size from the array
           },
         ],
       }),
     ],
+
     module: {
       rules: [
         {
           test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
-        },
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,  // Handle image files
-          type: 'asset/resource',
         },
         {
           test: /\.m?js$/,
@@ -61,7 +56,7 @@ module.exports = () => {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-transform-runtime'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
             },
           },
         },
